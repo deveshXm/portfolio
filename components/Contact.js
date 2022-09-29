deaab438568cfdf29aea0819dc8b3d11f58bb121
Devesh Meena <deveshm.cs.20@nitj.ac.in>
2022-09-29 14:30:24 +0530

    updated contact form

--- components/Contact.js
+++ components/Contact.js
@@ -8,16 +8,18 @@ const Contact = () => {
   return (
     <div className="bg-black h-fit p-12 text-white space-y-10">
       <div className="space-y-6">
-        <h1 className="text-4xl font-bold ">Contact Me</h1>
-        <div
-          className="font-roboto text-sm font-normal"
-          dangerouslySetInnerHTML={{ __html: subhead }}
-        />
+        <h1 className="text-4xl font-bold md:text-8xl">Contact Me</h1>
       </div>
-      <div className="md:flex md:space-x-10">
-        <ContactForm />
-        <div className="inline-block overflow-hidden border-gray-500 border-2">
-          <img src="/map.png" className=" scale-[2.0] md:scale-100" />
+      <div className="lg:flex ">
+        <div className="space-y-10 bg-[#27282c] p-10">
+          <div
+            className="font-roboto text-sm xl:text-lg font-normal"
+            dangerouslySetInnerHTML={{ __html: subhead }}
+          />
+          <ContactForm />
+        </div>
+        <div className="inline-block overflow-hidden">
+          <img src="/map.png" className=" scale-[2.0] md:scale-100 " />
         </div>
       </div>
     </div>
