c33c9078069fe289593ba27e4178f4b1ca9f73e1
Devesh Meena <deveshm.cs.20@nitj.ac.in>
2022-09-29 13:42:22 +0530

    contact and experience section

--- components/SubComponents/ExperienceCard.js
+++ components/SubComponents/ExperienceCard.js
@@ -1,32 +1,42 @@
 import { useState, React } from "react";
 
-const ExperienceCard = (props) => {
+const ExperienceCard = ({ item }) => {
   const [drop, setDrop] = useState(false);
 
   const handleClick = () => {
     setDrop(!drop);
   };
   return (
-    <div className="space-y-4 text-sm">
-      <div className="w-[90vw] bg-purple-600 h-12 flex items-center justify-between rounded-md text-white font-medium px-2">
-        <div>Microsoft Engage 2022</div>
+    <div className="space-y-4 text-sm md:text-md">
+      <div
+        className="w-[90vw] md:w-[60vw] bg-purple-600 h-12 flex items-center justify-between rounded-md text-white font-medium px-2 sm:px-6 hover:cursor-pointer"
+        onClick={handleClick}
+      >
+        <div>{item.title}</div>
         <div className="flex items-center space-x-2">
-          <div>May - June 2022</div>
-          <button
-            onClick={handleClick}
-            className="hover:text-[#61c4d9] hover:border-[#61c4d9]  w-6 rounded-full border-2 "
-          >
+          <div>{item.timeline}</div>
+          <div className="hover:text-[#61c4d9] text-center ">
             {drop ? "-" : "+"}
-          </button>
+          </div>
         </div>
       </div>
-      <div
-        className={`bg-[#241d41] ${
-          drop ? "down" : "up"
-        } text-white rounded-md p-4 w-[90vw]`}
-      >
-        <div></div>
-      </div>
+      {drop && (
+        <div
+          className={`bg-[#241d41] text-white rounded-md p-4 w-[90vw] md:w-[60vw] font-normal `}
+        >
+          <div>{item.about}</div>
+          <div className="flex space-x-2 mt-4">
+            {item.stack.map((tech) => (
+              <div
+                key={tech}
+                className="bg-blue-800 font-normal p-2 rounded-full"
+              >
+                {tech}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,26 @@
-import Head from 'next/head';
-import Expertise from '../components/Expertise';
-import Intro from '../components/Intro';
-import Work from '../components/Work';
-import NavBar from '../components/NavBar';
-import Experience from '../components/Experience';
+import Head from "next/head";
+import Expertise from "../components/Expertise";
+import Intro from "../components/Intro";
+import Work from "../components/Work";
+import NavBar from "../components/NavBar";
+import Experience from "../components/Experience";
+import Contact from "../components/Contact";
 
 export default function Home() {
-
   return (
-    <div className='body'>
+    <div className="body">
       <Head>
         <title>Portfolio</title>
         <meta name="deScription" content="Devesh Meena Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <NavBar/>
+      <NavBar />
       <Intro />
-      <Expertise/>
-      <Work/>
-      <Experience/>
-      <div className='h-screen bg-[#1a191d]'></div>
+      <Expertise />
+      <Work />
+      <Experience />
+      <Contact />
     </div>
-  )
+  );
 }
