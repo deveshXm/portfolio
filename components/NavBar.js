8877360f4579d21bdbad7ac573218655a0f5d741
Devesh Meena <deveshm.cs.20@nitj.ac.in>
2022-09-30 15:18:03 +0530

    navbar bug fixes

--- components/NavBar.js
+++ components/NavBar.js
@@ -16,24 +16,9 @@ const NavBar = ({
 
   const css =
     "hover:text-[#66d9ed] px-5 hover:cursor-pointer transform duration-300 ease-in-out";
+
+  const rescss = "flex border-b border-purple-600 py-4 pl-4 hover:text-[#66d9ed] px-5 hover:cursor-pointer transform duration-300 ease-in-out w-full"
   return (
-    // <div className="fixed h-[8vh] backdrop-blur-3xl z-[10] w-full flex items-center justify-center text-white text-sm font-medium font-roboto tracking-tighter ">
-    // <button onClick={handleHome} className={css}>
-    //   home
-    // </button>
-    // <button onClick={handleExpertise} className={css}>
-    //   expertise
-    // </button>
-    // <button onClick={handleWork} className={css}>
-    //   work
-    // </button>
-    // <button onClick={handleExperience} className={css}>
-    //   experience
-    // </button>
-    // <button onClick={handleContact} className={css}>
-    //   contact
-    // </button>
-    // </div>
     <div className="flex fixed top-0 z-[10] w-full text-white p-4 2xl:text-3xl font-bold justify-end bg-gradient-to-t from-transparent to-black md:bg-none md:backdrop-blur-3xl items-center md:justify-center ">
       <div className="hidden md:flex">
         <button onClick={handleHome} className={css}>
@@ -66,20 +51,20 @@ const NavBar = ({
             : "fixed left-[-100%] top-0 h-full bg-opacity-50 backdrop-blur-md bg-black w-60 border-r border-r-gray-500 z-[1000] ease-in duration-500 "
         }
       >
-        <ul className="pt-24 w-full pl-14">
-          <button onClick={handleHome} className={css}>
+        <ul className="pt-24">
+          <button onClick={handleHome} className={rescss}>
             home
           </button>
-          <button onClick={handleExpertise} className={css}>
+          <button onClick={handleExpertise} className={rescss}>
             expertise
           </button>
-          <button onClick={handleWork} className={css}>
+          <button onClick={handleWork} className={rescss}>
             work
           </button>
-          <button onClick={handleExperience} className={css}>
+          <button onClick={handleExperience} className={rescss}>
             experience
           </button>
-          <button onClick={handleContact} className={css}>
+          <button onClick={handleContact} className={rescss}>
             contact
           </button>
         </ul>
