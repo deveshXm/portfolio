a9f27d9a374bbe8ae32cd17f692da10283c8eb6c
Devesh Meena <deveshm.cs.20@nitj.ac.in>
2022-09-30 12:12:42 +0530

    bug fixes

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='h-[30vh] bg-[#27282c]'>
-    <div>
-      
+    <div className="h-[30vh] bg-[#27282c]">
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
