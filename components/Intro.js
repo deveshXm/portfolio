2fc871cb791a7579728d04921dfba01f8425cefd
Devesh Meena <deveshm.cs.20@nitj.ac.in>
2022-10-01 12:22:45 +0530

    button for resume added

--- components/Intro.js
+++ components/Intro.js
@@ -29,16 +29,28 @@ const Intro = ({ scroll }) => {
   return (
     <div ref={scroll}>
       <div
-        className="h-screen w-full flex items-center justify-center"
+        className=" bg-[#15212c] h-screen w-full flex items-center justify-center"
         ref={vantaRef}
       >
-        <div className="block-inline z-[1] text-center  text-white  ">
-          <h6 className="text-6xl sm:text-7xl lg:text-8xl xl:text-9xl font-bold drop-shadow-2xl max-w-[90vw]">
-            DEVESH MEENA
-          </h6>
-          <h4 className="text-1xl lg:text-3xl font-semibold font-roboto drop-shadow-2xl">
-            STUDENT , FULL STACK DEVELOPER{" "}
-          </h4>
+        <div className="space-y-20">
+          <div className="space-y-20">
+            <div className=" z-[1] text-center  text-white mt-36 ">
+              <h6 className="text-6xl sm:text-7xl lg:text-8xl xl:text-9xl font-bold drop-shadow-2xl max-w-[90vw]">
+                DEVESH MEENA
+              </h6>
+              <h4 className="text-1xl lg:text-3xl font-semibold font-roboto drop-shadow-2xl">
+                STUDENT , FULL STACK DEVELOPER{" "}
+              </h4>
+            </div>
+            <div className="flex justify-center ">
+              <a href="https://drive.google.com/file/d/1ZAiKj7vhU_x7Zu5JTGWpSd8q33jaAVSP/view?usp=sharing">
+                <button className="download">RESUME</button>
+              </a>
+            </div>
+          </div>
+            <div className="mx-auto w-fit h-fit mt-auto">
+              <div class="indicator"/>
+            </div>
         </div>
       </div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/button.scss'
+import '../styles/scroll.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
