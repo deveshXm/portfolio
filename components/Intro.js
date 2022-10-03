6d9cacdbe781c2afe09fe0cca1ddd2c8f312f561
devesh <nothefakedevesh@gmail.com>
2022-10-03 14:10:20 +0530

    fixes

--- components/Intro.js
+++ components/Intro.js
@@ -17,25 +17,32 @@ const Intro = ({ scroll }) => {
         <li></li>
         <li></li>
         <li></li>
-        <div className="text-white min-h-screen h-fit w-screen md:flex items-center justify-around ">
-          <div className=" md:ml-20 space-y-10 md:space-y-4  ">
-            <div className=" font-poppins  text-4xl md:text-8xl font-medium text-lightblue flex justify-center">
-              <Heading title={"Hi! I'm Devesh"} />
+        <div className="text-white min-h-screen h-fit w-screen flex items-center justify-center ">
+          <div className="w-full md:flex md:items-center md:justify-around">
+            <div className=" md:ml-20 space-y-10 md:space-y-4 ">
+              <div className=" font-poppins  text-4xl md:text-8xl font-medium text-lightblue flex justify-center">
+                <Heading title={"Hi! I'm Devesh"} />
+              </div>
+              <div className="mt-5 font-poppins text-3xl text-center">
+                <p>Student , Developer & Basketball player.</p>
+              </div>
+              <div className="flex justify-center ">
+                <Button
+                  title={"RESUME"}
+                  link={
+                    "https://drive.google.com/file/d/1ZAiKj7vhU_x7Zu5JTGWpSd8q33jaAVSP/view?usp=sharing"
+                  }
+                />
+              </div>
             </div>
-            <div className="mt-5 font-poppins text-3xl text-center">
-              <p>Student , Developer & Basketball player.</p>
-            </div>
-            <div className="flex justify-center ">
-              <Button
-                title={"RESUME"}
-                link={
-                  "https://drive.google.com/file/d/1ZAiKj7vhU_x7Zu5JTGWpSd8q33jaAVSP/view?usp=sharing"
-                }
+            <div>
+              <Image
+                src="/student.png"
+                height={400}
+                width={400}
+                alt="student"
               />
             </div>
-          </div>
-          <div>
-            <Image src="/student.png" height={400} width={400} alt="student" />
           </div>
         </div>
       </ul>
